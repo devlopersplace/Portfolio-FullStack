1174e31e327ca52592bf8bd2e1326712d6afff76
developers-place <devlopersplace@gmail.com>
2026-02-28 18:08:46 +0530

    feat: switch to PostgreSQL and dynamic port for Render

--- portfolio/src/main/java/com/himanshu/portfolio/controller/ProjectController.java
+++ portfolio/src/main/java/com/himanshu/portfolio/controller/ProjectController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/projects")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://himanshumishra.net")
 public class ProjectController {
 	private final ProjectService projectService;
 	
